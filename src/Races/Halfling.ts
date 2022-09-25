9e4c9ecf89f3c968b8dd8020bfc4621a4a647d4e
ioott <vioott@gmail.com>
2022-09-25 16:11:34 -0300

    feat: cria as classes Dwarf, Elf, Halfling e Orc, que herdam de Race

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  protected _name: string;
+  protected _dexterity: number;
+  private _maxLifePoints = 60;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    this.counter += 1;
+    return this.counter;
+  }
+}
--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,23 +1,11 @@
-// Cada raça terá seu número máximo de instâncias, que será definido dentro de cada classe especializada;
-// Na classe Race, o método deve lançar um erro com a mensagem Not implemented.
-// Esse número corresponde à quantidade máxima de pontos de vida da raça;
-// Cada raça terá seu número máximo de pontos, que será definido dentro de cada classe especializada;
-
 export default abstract class Race {
-  private readonly _name: string;
-  private readonly _dexterity: number;
-
-  private static counter = 0;
+  protected _name: string;
+  protected _dexterity: number;
+  protected static counter = 0;
 
   constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
-  }
-
-  static createdRacesInstances(): number {
-    Race.counter += 1;
-    throw new Error('Not implemented');
-    return Race.counter;
   }
 
   abstract get maxLifePoints(): number;
@@ -28,5 +16,9 @@ export default abstract class Race {
 
   get dexterity(): number {
     return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
   }
 }
