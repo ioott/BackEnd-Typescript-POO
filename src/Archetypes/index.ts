2084db7e4f2829d0775ab4a0780ffc5c19f90b52
ioott <vioott@gmail.com>
2022-09-25 19:14:24 -0300

    feat: cria a super classe abstrata Archetype

---
+++ src/Archetypes/index.ts
@@ -0,0 +1,8 @@
+import Archetype from './Archetype';
+// import Orc from './Orc';
+// import Dwarf from './Dwarf';
+// import Elf from './Elf';
+// import Halfling from './Halfling';
+
+export default Archetype;
+// export { Orc, Dwarf, Elf, Halfling };
